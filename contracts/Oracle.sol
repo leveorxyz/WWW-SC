29c504ee0310eadef6384a111ba0db3c99caabf0
Tahlil <bsse0803@iit.du.ac.bd>
2022-08-23 19:47:36 +0600

    Check buy and sell usd tx

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -27,4 +27,24 @@ contract Protocol is Ownable{
         _sellUSDTxIDs.push(sellUSDTx);
     }
 
+    function checkBuyTx(uint256 buyUSDTx) external onlyERC20 returns(bool) {
+        for (uint256 index = 0; index < _buyUSDTxIDs.length; index++) {
+            if (_buyUSDTxIDs[index] == buyUSDTx) {
+                delete _buyUSDTxIDs[index];
+                return true;
+            } 
+        }
+        return false;
+    }
+
+    function checkSellTx(uint256 sellUSDTx) external onlyERC20 returns(bool) {
+        for (uint256 index = 0; index < _sellUSDTxIDs.length; index++) {
+            if (_sellUSDTxIDs[index] == sellUSDTx) {
+                delete _buyUSDTxIDs[index];
+                return true;
+            } 
+        }
+        return false;
+    }
+
 }
