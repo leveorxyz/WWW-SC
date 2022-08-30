39ef06b407514562e138395d95e1f885e4d96094
Tahlil <bsse0803@iit.du.ac.bd>
2022-08-31 03:53:53 +0600

    Update oracle interface

--- contracts/interfaces/IOracle.sol
+++ contracts/interfaces/IOracle.sol
@@ -6,4 +6,5 @@ interface IOracle {
     function initialize(address _erc20Address) external;
     function checkBuyTx(uint256 buyUSDTx, uint256 amount) external returns(bool);
     function checkSellTx(uint256 sellUSDTx, uint256 amount) external returns(bool); 
+    function checkRentTx(uint256 rentUSDTx, uint256 amount) external returns(bool);
 }
