a84d56d7c748449e1b777a678f055e9842cffc07
Tahlil <bsse0803@iit.du.ac.bd>
2022-09-05 12:07:07 +0600

    Create instance of all three contracts

--- test/index.ts
+++ test/index.ts
@@ -8,6 +8,7 @@ import OracleArtifacts from '../artifacts/contracts/Oracle.sol/Oracle.json';
 import {Oracle} from '../typechain/contracts/Oracle';
 import ProtocolArtifacts from '../artifacts/contracts/Protocol.sol/Protocol.json';
 import {Protocol} from '../typechain/contracts/Protocol';
+import { LandingToken__factory } from '../typechain/factories/contracts/LandingToken__factory';
 
 const {deployContract} = waffle;
 
@@ -16,25 +17,28 @@ describe("Greeter test", function () {
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshopt in every test.
   async function deployOnceFixture() {
+    const initTimestamp: number = 1661990400; // UTC timestamp, Sept 1st, 2022, 12:00 am 
     let landingToken: LandingToken;
-    let oracle: LandingToken;
+    let oracle: Oracle;
     let protocol: Protocol;
     // Contracts are deployed using the first signer/account by default
     const [owner, ...otherAccounts] = await ethers.getSigners();
 
-    landingToken = (await deployContract(owner, LandingTokenArtifacts, ['Hello, world!'])) as LandingToken;
-  
-    return { landingToken, owner, otherAccounts };
+    oracle = (await deployContract(owner, LandingTokenArtifacts)) as Oracle;
+    protocol = (await deployContract(owner, LandingTokenArtifacts, [oracle.address, initTimestamp])) as Protocol;
+    landingToken = LandingToken__factory.connect(await protocol.getLandingTokenAddress(), owner) as LandingToken;
+    
+    return {  owner, otherAccounts, protocol, landingToken, oracle };
   }
 
   describe("Test suite", function () {
     it("Test 1", async function () {
-      const { greeter, owner } = await loadFixture(deployOnceFixture);
+      const { owner } = await loadFixture(deployOnceFixture);
       expect(true);
     });
 
     it("Test 2", async function () {
-      const { greeter, owner } = await loadFixture(deployOnceFixture);
+      const {  owner } = await loadFixture(deployOnceFixture);
       expect(1).to.equal(1);
     });
 
