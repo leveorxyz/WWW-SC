d0e995d4e7f9c1cb5f9b237180bcfc7d99169899
Tahlil <bsse0803@iit.du.ac.bd>
2022-09-06 15:20:32 +0600

    Check allowance for selling token

--- test/index.ts
+++ test/index.ts
@@ -95,7 +95,7 @@ describe("Landing token test suite", function () {
       await tx.wait();
       tx = await protocol.buyLANDC(usdAmount, txID);
       await tx.wait();
-      console.log(await getAllowance(landingToken, owner.address, protocol.address));
+      expect(await getAllowance(landingToken, owner.address, protocol.address)).to.eq(96);
       
       expect(await getBalance(landingToken, owner.address)).to.eq(96);
       expect(await getBalance(landingToken, landingToken.address)).to.eq(999999999900);
@@ -105,10 +105,7 @@ describe("Landing token test suite", function () {
       await tx.wait();
       tx = await protocol.sellLANDC(1, usdAmount, txID);
       await tx.wait();
-      console.log(await getAllowance(landingToken, owner.address, protocol.address));
-
-      console.log(await getAllowance(landingToken, owner.address, protocol.address));
-
+      expect(await getAllowance(landingToken, owner.address, protocol.address)).to.eq(5.999999999640001);
       expect(await getBalance(landingToken, owner.address)).to.eq(5.999999999640001);
       expect(await getBalance(landingToken, landingToken.address)).to.eq(999999999990);
     });
